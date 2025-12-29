c02df3a634bfb7f8039a3937287385c26a602713
Rahul-2415 <rahulsingh8171335162@gmail.com>
2025-12-29 12:55:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+const colorSelect = document.getElementById("colorSelect");
+const removeButton = document.querySelector('input[type="button"]');
+
+
+function removeSelectedColor() {
+  const selectedIndex = colorSelect.selectedIndex;
+  if (selectedIndex >= 0) {
+    colorSelect.remove(selectedIndex);
+  }
+}
+
+
+removeButton.addEventListener("click", removeSelectedColor);
+
